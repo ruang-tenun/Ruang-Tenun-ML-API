82d2a098471370967f120daec938d5afa229c189
rizan <rizanurmaulana2810@gmail.com>
2024-12-02 22:18:37 +0700

    initiate commit

---
+++ app.py
@@ -0,0 +1,11 @@
+from flask import Flask
+
+# Define a Flask app
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return 'Hello World!'
+
+if __name__ == '__main__':
+    app.run(debug=True)
